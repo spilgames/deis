55279949e65888e508a7ce306a5c4e0d8dce8f33
Olivier Korver <Olivier.Korver@spilgames.com>
2014-09-26 16:00:21 +0200

    Opensrack provisioning cluster script changes

--- contrib/openstack/provision-openstack-cluster.sh
+++ contrib/openstack/provision-openstack-cluster.sh
@@ -31,7 +31,7 @@ if ! which neutron > /dev/null; then
 fi
 
 if [ -z "$3" ]; then
-  FLAVOR="m1.large"
+  FLAVOR="DEIS"
 else
   FLAVOR=$3
 fi
@@ -41,21 +41,21 @@ if [ -z "$OS_AUTH_URL" ]; then
   exit 1
 fi
 
-if ! nova network-list|grep -q deis &>/dev/null; then
-  echo_yellow "Creating deis private network..."
-  CIDR=${DEIS_CIDR:-10.21.12.0/24}
-  SUBNET_OPTIONS=""
-  [ ! -z "$DEIS_DNS" ] && SUBNET_OPTIONS=$(echo $DEIS_DNS|awk -F "," '{for (i=1; i<=NF; i++) printf "--dns-nameserver %s ", $i}')
-  NETWORK_ID=$(neutron net-create deis | awk '{ printf "%s", ($2 == "id" ? $4 : "")}')
-  echo "DBG: SUBNET_OPTIONS=$SUBNET_OPTIONS"
-  SUBNET_ID=$(neutron subnet-create --name deis_subnet $SUBNET_OPTIONS deis $CIDR| awk '{ printf "%s", ($2 == "id" ? $4 : "")}')
-else
-  NETWORK_ID=$(neutron net-list | awk '{printf "%s", ($4 == "deis" ? $2 : "")}')
-fi
+#if ! nova network-list|grep -q net_deis &>/dev/null; then
+#  echo_yellow "Creating deis private network..."
+#  CIDR=${DEIS_CIDR:-10.21.12.0/24}
+#  SUBNET_OPTIONS=""
+#  [ ! -z "$DEIS_DNS" ] && SUBNET_OPTIONS=$(echo $DEIS_DNS|awk -F "," '{for (i=1; i<=NF; i++) printf "--dns-nameserver %s ", $i}')
+#  NETWORK_ID=$(neutron net-create net_deis | awk '{ printf "%s", ($2 == "id" ? $4 : "")}')
+#  echo "DBG: SUBNET_OPTIONS=$SUBNET_OPTIONS"
+#  SUBNET_ID=$(neutron subnet-create --name deis_subnet $SUBNET_OPTIONS deis $CIDR| awk '{ printf "%s", ($2 == "id" ? $4 : "")}')
+#else
+  NETWORK_ID=$(neutron net-list | awk '{printf "%s", ($4 == "net_deis" ? $2 : "")}')
+#fi
 
 
 if [ -z "$DEIS_NUM_INSTANCES" ]; then
-    DEIS_NUM_INSTANCES=3
+    DEIS_NUM_INSTANCES=5
 fi
 
 # check that the CoreOS user-data file is valid
